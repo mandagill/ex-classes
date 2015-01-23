deb725c3bc2fc575808c1a7f0dab7bbd5040f60b
Hackbright Student <info@hackbrightacademy.com>
2015-01-22 17:50:38 -0800

    finished melons as seperate classes

--- melons.py
+++ melons.py
@@ -2,11 +2,11 @@
 
 class Watermelon(object):
     def __init__(self):
-        name = "watermelon"
-        color = "green"
-        origin = "domestic" #we may want to make this a boolean later for easier price calculating, will see how that goes 
-        shape = "natural"
-        seasons = ['Fall', 'Summer']
+        self.name = "watermelon"
+        self.color = "green"
+        self.origin = "domestic" #we may want to make this a boolean later for easier price calculating, will see how that goes 
+        self.shape = "natural"
+        self.seasons = ['Fall', 'Summer']
 
     def get_price(self,qty):
         pricetag = 5.0
@@ -19,11 +19,11 @@ class Watermelon(object):
 
 class Cantaloupe(object):
     def __init__(self):
-        name = "cantaloupe"
-        color = "tan"
-        origin = "domestic" #we may want to make this a boolean later for easier price calculating, will see how that goes 
-        shape = "natural"
-        seasons = ['Spring', 'Summer']
+        self.name = "cantaloupe"
+        self.color = "tan"
+        self.origin = "domestic" #we may want to make this a boolean later for easier price calculating, will see how that goes 
+        self.shape = "natural"
+        self.seasons = ['Spring', 'Summer']
 
     def get_price(self,qty):
         pricetag = 5.0
@@ -34,3 +34,93 @@ class Cantaloupe(object):
         return price
 
 
+class Casaba(object):
+    def __init__(self):
+        self.name = "casaba"
+        self.color = "green"
+        self.origin = "imported" #we may want to make this a boolean later for easier price calculating, will see how that goes 
+        self.shape = "natural"
+        self.seasons = ['Spring', 'Summer', 'Fall', 'Winter']
+
+    def get_price(self,qty):
+        pricetag = 9.0
+        return pricetag*qty
+
+class Sharlyn(object):
+    def __init__(self):
+        self.name = "sharlyn"
+        self.color = "tan"
+        self.origin = "imported" #we may want to make this a boolean later for easier price calculating, will see how that goes 
+        self.shape = "natural"
+        self.seasons = ['Summer']
+
+    def get_price(self,qty):
+        pricetag = 5.0*1.5
+        return pricetag*qty
+
+
+class SantaClaus(object):
+    def __init__(self):
+        self.name = "santa claus"
+        self.color = "green"
+        self.origin = "imported" #we may want to make this a boolean later for easier price calculating, will see how that goes 
+        self.shape = "natural"
+        self.seasons = ['Winter', 'Spring']
+
+    def get_price(self,qty):
+        pricetag = 7.5
+        return pricetag*qty
+
+
+class Christmas(object):
+    def __init__(self):
+        self.name = "christmas"
+        self.color = "green"
+        self.origin = "domestic" #we may want to make this a boolean later for easier price calculating, will see how that goes 
+        self.shape = "natural"
+        self.seasons = ['Winter']
+
+    def get_price(self,qty):
+        pricetag = 5
+        return pricetag*qty
+
+
+class HornedMelon(object):
+    def __init__(self):
+        self.name = "horned melon"
+        self.color = "yellow"
+        self.origin = "imported" #we may want to make this a boolean later for easier price calculating, will see how that goes 
+        self.shape = "natural"
+        self.seasons = ['Summer']
+
+    def get_price(self,qty):
+        pricetag = 7.5
+        return pricetag*qty
+
+
+class Xigua(object):
+    def __init__(self):
+        self.name = "xigua"
+        self.color = "black"
+        self.origin = "imported" #we may want to make this a boolean later for easier price calculating, will see how that goes 
+        self.shape = "square"
+        self.seasons = ['Summer']
+
+    def get_price(self,qty):
+        # This is one bougie melon. The Amex Black of melons. 
+        pricetag = 5.0 * 1.5 * 2
+        return pricetag * qty
+
+
+class Ogen(object):
+    def __init__(self):
+        self.name = "ogen"
+        self.color = "tan"
+        self.origin = "domestic" #we may want to make this a boolean later for easier price calculating, will see how that goes 
+        self.shape = "natural"
+        self.seasons = ['Spring','Summer']
+
+    def get_price(self,qty):
+        # This is one bougie melon. The Amex Black of melons. 
+        pricetag = 6.0
+        return pricetag * qty
